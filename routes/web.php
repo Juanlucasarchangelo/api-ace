30911ee0946478f49ca4e329a84275fd037e909a
juanlucasarchangelo <jlarchangelo@outlook.com>
2025-02-13 18:04:16 -0300

    Recomeço do projeto sem vue

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/listar/cliente', [App\Http\Controllers\ClienteController::class, 'show'])->name('listar-cliente');
-
 Route::get('/cadastrar/site', [App\Http\Controllers\SiteController::class, 'show'])->name('cadastrar-site');
 Route::get('/cadastrar/cliente', [App\Http\Controllers\ClienteController::class, 'show'])->name('cadastrar-cliente');
-
 Route::get('/painel', [App\Http\Controllers\PainelController::class, 'index'])->name('painel');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
